8a9e8d1f0be7feca20f276a0395f93f60aba2260
brad-basilio <brad.basilioppy@gmail.com>
2025-04-16 10:58:59 -0500

    add

--- app/Helpers/EmailConfig.php
+++ app/Helpers/EmailConfig.php
@@ -3,36 +3,23 @@
 namespace App\Helpers;
 
 use PHPMailer\PHPMailer\PHPMailer;
-use App\Models\General; // Asegúrate de importar tu modelo General
 
 class EmailConfig
 {
-    static function config($name, $mensaje, $fromAddress = null, $fromName = null): PHPMailer
+    static function config($name, $mensaje): PHPMailer
     {
         $mail = new PHPMailer(true);
-        
-        // Configuración SMTP (mejor usar config() en lugar de valores hardcodeados)
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
         $mail->isSMTP();
-        $mail->Host = config('mail.mailers.smtp.host');
+        $mail->Host = 'mail.qallpa.pe';
         $mail->SMTPAuth = true;
-        $mail->Username = config('mail.mailers.smtp.username');
-        $mail->Password = config('mail.mailers.smtp.password');
-        $mail->SMTPSecure = config('mail.mailers.smtp.encryption');
-        $mail->Port = config('mail.mailers.smtp.port');
-        
-        // Asunto y charset
-        $mail->Subject = $name . ', ' . $mensaje;
+        $mail->Username = 'hola@qallpa.pe';
+        $mail->Password = '}+a2sco8jhvf';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port = 465;
+        $mail->Subject = '' . $name . ', ' . $mensaje;
         $mail->CharSet = 'UTF-8';
-        
-        // Configuración dinámica del remitente
-        $fromEmail = $fromAddress ?? General::first()->email ?? config('mail.from.address');
-        $fromName = $fromName ?? General::first()->name ?? config('mail.from.name');
-        
-        $mail->setFrom($fromEmail, $fromName);
-        
-        // Opcional: Configurar reply-to igual que from
-        $mail->addReplyTo($fromEmail, $fromName);
-        
+        $mail->setFrom('hola@qallpa.pe', 'Qallpa');
         return $mail;
     }
 }
